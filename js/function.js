cf326cc13a8ab5bb6e81e3b8b9545ea40b788d37
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2018-08-16 16:20:07 -0500

    adds FAQ page

--- js/function.js
+++ js/function.js
@@ -35,3 +35,38 @@
   });
 
 })(jQuery); // End of use strict
+
+// $(document).ready( function () {
+//   console.log('mailchimp!')
+//     // I only have one form on the page but you can be more specific if need be.
+//     var $form = $('form');
+//         $('form input[type="submit"]').bind('click', function ( event ) {
+//           event.preventDefault();
+//             // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
+
+//             register($form)
+//             //if ( validate_input($form) ) { register($form); }
+//         });
+// });
+
+// function register($form) {
+
+//     $.ajax({
+//         type: $form.attr('method'),
+//         url: $form.attr('action'),
+//         data: $form.serialize(),
+//         cache       : false,
+//         dataType    : 'json',
+//         contentType: "application/json; charset=utf-8",
+//         error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+//         success     : function(data) {
+//             if (data.result != "success") {
+//               console.log(data.msg)                // Something went wrong, do something to notify the user. maybe alert(data.msg);
+//             } else {
+//               console.log(data.msg) 
+//                 console.log('yay!')
+//             }
+//         }
+//     });
+// }
+
