476b0da08ab4086ec0b1d694e1300bc9ca8142d3
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2018-08-23 13:05:41 -0500

    updates pack sequence structure

--- js/lounge.js
+++ js/lounge.js
@@ -31,143 +31,143 @@ var betaInfo = [{
   "email": "abby.tracy2012@gmail.com",
   "first_name": "Abby",
   "sequence_type": "cycle",
-  "pack_sequence": 
+  "pack_sequence": [
     {
       "first_pack": "blah",
       "second_pack": "orange",
       "third_pack": "pomelo",
       "fourth_pack": "lime"
-    },
+    }],
     "personal_hint": ""
   },
   {
     "email": "sedelste@gmail.com",
     "first_name": "Shanleigh",
     "sequence_type": "cycle",
-    "pack_sequence": 
+    "pack_sequence": [
       {
         "first_pack": "blah",
         "second_pack": "orange",
         "third_pack": "pomelo",
         "fourth_pack": "lime"
-      },
+      }],
       "personal_hint": ""
     },
     {
       "email": "shonaliditz@gmail.com",
       "first_name": "Shonali",
       "sequence_type": "cycle",
-      "pack_sequence": 
+      "pack_sequence": [
         {
           "first_pack": "blah",
           "second_pack": "orange",
           "third_pack": "pomelo",
           "fourth_pack": "lime"
-        },
+        }],
         "personal_hint": ""
       },
       {
         "email": "tillywinx@gmail.com",
         "first_name": "Tilly",
         "sequence_type": "cycle",
-        "pack_sequence": 
+        "pack_sequence": [
           {
             "first_pack": "blah",
             "second_pack": "orange",
             "third_pack": "pomelo",
             "fourth_pack": "lime"
-          },
+          }],
           "personal_hint": ""
         },
   {
     "email": "raissanfg@gmail.com",
     "first_name": "Raissa",
     "sequence_type": "cycle",
-    "pack_sequence": 
+    "pack_sequence": [
       {
         "first_pack": "blah",
         "second_pack": "orange",
         "third_pack": "pomelo",
         "fourth_pack": "lime"
-      },
+      }],
       "personal_hint": ""
     },
   {
     "email": "phirany.lim@gmail.com",
     "first_name": "Phirany",
     "sequence_type": "cycle",
-    "pack_sequence": 
+    "pack_sequence": [
       {
         "first_pack": "blah",
         "second_pack": "orange",
         "third_pack": "pomelo",
         "fourth_pack": "lime"
-      },
+      }],
       "personal_hint": ""
     },
   {
     "email": "maxinenicolemua@gmail.com",
     "first_name": "Maxine",
     "sequence_type": "cycle",
-    "pack_sequence": 
+    "pack_sequence": [
       {
         "first_pack": "blah",
         "second_pack": "orange",
         "third_pack": "pomelo",
         "fourth_pack": "lime"
-      },
+      }],
       "personal_hint": ""
     },
   {
     "email": "lundgren.jl@gmail.com",
     "first_name": "Jessica",
     "sequence_type": "cycle",
-    "pack_sequence": 
+    "pack_sequence": [
       {
         "first_pack": "blah",
         "second_pack": "orange",
         "third_pack": "pomelo",
         "fourth_pack": "lime"
-      },
+      }],
       "personal_hint": ""
     },
   {
     "email": "Kimmah.shah@gmail.com",
     "first_name": "Kimmah",
     "sequence_type": "cycle",
-    "pack_sequence": 
+    "pack_sequence": [
       {
         "first_pack": "blah",
         "second_pack": "orange",
         "third_pack": "pomelo",
         "fourth_pack": "lime"
-      },
+      }],
       "personal_hint": ""
     },
   {
     "email": "elizabethannbarton1@gmail.com",
     "first_name": "Liz",
     "sequence_type": "cycle",
-    "pack_sequence": 
+    "pack_sequence": [
       {
         "first_pack": "blah",
         "second_pack": "orange",
         "third_pack": "pomelo",
         "fourth_pack": "lime"
-      },
+      }],
       "personal_hint": ""
     },
   {
   "email": "amjordan92@gmail.com",
   "first_name": "Alexis",
   "sequence_type": "cycle",
-  "pack_sequence": 
+  "pack_sequence": [
     {
       "first_pack": "blah",
       "second_pack": "orange",
       "third_pack": "pomelo",
       "fourth_pack": "lime"
-    },
+    }],
     "personal_hint": "your personal instructions"
 }];
 
